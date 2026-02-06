f8ca91bce17b761aa537287d854b99aa8d7e391b
meguedes <guedesellen91@gmail.com>
2026-02-06 16:55:51 -0300

    Exercicos

---
+++ fundamentos/ExercicosFundamentos.java
@@ -0,0 +1,28 @@
+package fundamentos;
+import java.util.Scanner;
+
+public class ExercicosFundamentos {
+    public static void main(String[] args){
+        /*1 - Escreva um código que receba o nome e o ano de nascimento de alguém e imprima na tela a seguinte mensagem "Olá 'Fulano' você tem 'X' anos" 
+        var scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        var name = scanner.next();
+        System.out.print("Digite seu ano de nascimento: ");
+        var year = scanner.nextInt();
+        System.out.printf("Olá %s, você tem %s anos!", name, (2026 - year));
+        */
+
+        /* 2 - Escreva um código que receba o tamanho do lado de um quadrado, calcule sua área e exiba na tela (fórmula: área = base * altura).
+        var scanner = new Scanner(System.in);
+        System.out.print("Digite o tamanho do lado do quadrado: ");
+        var side = scanner.nextDouble();
+        var area = side * side;
+        System.out.printf("A área do quadrado é: %f", area);
+        */
+
+        /*3 - Escreva um código que receba a base e a altura de um retangulo, calcule a sua área e exiba na tela (fórmula: área = base * altura). */
+        var scanner = new Scanner(System.in);
+        System.out.print("Digite a base do retângulo: ");
+        var base = scanner.nextDouble();
+    }
+}
