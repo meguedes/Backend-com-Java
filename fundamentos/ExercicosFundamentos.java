90e0be5cfc9a485f2133c61e66acc1df70c067b7
meguedes <guedesellen91@gmail.com>
2026-02-09 12:47:03 -0300

    Exercícios sobre os fundamentos da linguagem

--- fundamentos/ExercicosFundamentos.java
+++ fundamentos/ExercicosFundamentos.java
@@ -1,16 +1,18 @@
 package fundamentos;
+import java.time.OffsetDateTime;
 import java.util.Scanner;
 
 public class ExercicosFundamentos {
     public static void main(String[] args){
-        /*1 - Escreva um código que receba o nome e o ano de nascimento de alguém e imprima na tela a seguinte mensagem "Olá 'Fulano' você tem 'X' anos" 
+        /*1 - Escreva um código que receba o nome e o ano de nascimento de alguém e imprima na tela a seguinte mensagem "Olá 'Fulano' você tem 'X' anos"*/
+        var baseYear = OffsetDateTime.now().getYear();
         var scanner = new Scanner(System.in);
         System.out.print("Digite seu nome: ");
         var name = scanner.next();
         System.out.print("Digite seu ano de nascimento: ");
         var year = scanner.nextInt();
-        System.out.printf("Olá %s, você tem %s anos!", name, (2026 - year));
-        */
+        System.out.printf("Olá %s, você tem %s anos!", name, (baseYear - year));
+        
 
         /* 2 - Escreva um código que receba o tamanho do lado de um quadrado, calcule sua área e exiba na tela (fórmula: área = base * altura).
         var scanner = new Scanner(System.in);
@@ -20,9 +22,22 @@ public class ExercicosFundamentos {
         System.out.printf("A área do quadrado é: %f", area);
         */
 
-        /*3 - Escreva um código que receba a base e a altura de um retangulo, calcule a sua área e exiba na tela (fórmula: área = base * altura). */
+        /*3 - Escreva um código que receba a base e a altura de um retangulo, calcule a sua área e exiba na tela (fórmula: área = base * altura). 
         var scanner = new Scanner(System.in);
         System.out.print("Digite a base do retângulo: ");
         var base = scanner.nextDouble();
+        System.out.print("Digite a altura do retângulo: ");
+        var altura = scanner.nextDouble();
+        var area = base * altura;
+        System.out.printf("A área do retângulo é: %.2f", area);
+        */
+
+        /*4 - Escreva um código  que receba o nome e a idade de 2 pessoas e imprima a diferença de idade entre elas
+        var scanner = new Scanner(System.in);
+        System.out.print("Digite a primeira idade: ");
+        var age1 = scanner.nextInt();
+        System.out.print("Digite a segunda idade:");
+        var age2 = scanner.nextInt();
+        System.out.printf("A diferença das idades é: %s", Math.abs(age1 - age2));*/
     }
 }
